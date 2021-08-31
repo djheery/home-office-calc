be470b58b76235209e5e3d09876075f6db4faf30
user <user@users-iMac.local>
2021-08-31 11:49:28 +0100

    SEISS Calc finished

--- src/seiss-app.js
+++ src/seiss-app.js
@@ -1,14 +1,20 @@
 const SEISS_APP = (() => {
   const ui = SEISS_UI.getSelectors()
+  const state = SEISS_STATE.getState()
   const loadEventListeners = () => {
     ui.inputs.forEach(i => i.addEventListener('blur', e => {
       UpdateState(i.dataset.name, parseFloat(i.value))
+      SEISS_UI.checkForErrors(state.affectedTurnover, state.averageTurnover)
+      if(i.parentElement.classList.contains('input-error')) return
+      const calculationResults = SEISS_CALCULATIONS.calculatePercentageChange(state.averageTurnover, state.affectedTurnover)
+      SEISS_UI.displayResults(calculationResults, false)
     }))
     window.addEventListener('DOMContentLoaded', e => {
       for(let i = 0; i < ui.inputs.length; i++) {
         UpdateState(ui.inputs[i].dataset.name, parseFloat(ui.inputs[i].value))
       }
-      SEISS_CALCULATIONS.calculatePercentageChange()
+      const calculationResults = SEISS_CALCULATIONS.calculatePercentageChange(state.averageTurnover, state.affectedTurnover)
+      SEISS_UI.displayResults(calculationResults, false)
     })
     ui.tooltips.forEach(tt => tt.addEventListener('click', SEISS_UI.displayToolTip))
   }
@@ -21,6 +27,7 @@ const SEISS_APP = (() => {
   return {
     init: () => {
       loadEventListeners()
+      console.log(typeof 1)
     }
   }
 })(SEISS_CALCULATIONS, SEISS_UI, SEISS_STATE)
